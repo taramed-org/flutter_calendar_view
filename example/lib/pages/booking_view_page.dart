3aeb9519f16b03ccfd2be35c7055ce48a036fe94
Muslimin Ontong <moshOntong@gmail.com>
2023-10-14 13:53:36 +0800

    remove unecessary package

--- example/lib/pages/booking_view_page.dart
+++ example/lib/pages/booking_view_page.dart
@@ -1,5 +1,4 @@
 import 'package:calendar_view/calendar_view.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
